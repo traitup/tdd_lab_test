07ec1f4e44c88cfe026dc5e0ad1d5ef048717f4e
traitup <115389401+traitup@users.noreply.github.com>
2023-03-22 13:38:08 +0700

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World1"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/hello/{name}")
+def read_name(name: str = None):
+    return {"hello": name}
+
+handler = Mangum(app)
