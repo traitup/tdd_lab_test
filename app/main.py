23406eb6ca6469ff2230ccb0d875b23fe7662776
traitup <115389401+traitup@users.noreply.github.com>
2023-03-22 14:16:11 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def read_item(item_id: int, q: str = None):
 def read_name(name: str = None):
     return {"hello": name}
 
-@app.post("/callname/{name}")
+@app.post("/callname")
 def read_name(name: str = None):
     return {"hello": name}
 
